064f8496042500933e6164b4644270479556b19d
Frédéric MOHIER <frederic.mohier@gmail.com>
2015-09-25 14:09:26 +0000

    Package PyPi version 0.1.1

--- README.rst
+++ README.rst
@@ -1,6 +1,8 @@
-# alignak_backend_client
-
+Package alignak_backend_client
+==============================
 [![Build Status](https://travis-ci.org/Alignak-monitoring-contrib/alignakbackend-api-client.svg?branch=master)](https://travis-ci.org/Alignak-monitoring-contrib/alignakbackend-api-client)
 [![Coverage Status](https://coveralls.io/repos/Alignak-monitoring-contrib/alignak-backend-client/badge.svg?branch=master&service=github)](https://coveralls.io/github/Alignak-monitoring-contrib/alignak-backend-client?branch=master)
 
 Alignak backend client
+
+This package contains a Python class to use for interactions with Alignak backend REST API.
--- setup.py
+++ setup.py
@@ -3,10 +3,26 @@
 
 from setuptools import setup
 
+import os
+del os.link
+
 setup(
-    name='alignak_backend_client',
-    version='0.1.1',
-    url='https://github.com/Alignak-monitoring-contrib/alignak-backend-client',
-    setup_requires=['pbr'],
-    pbr=True,
+    name = "alignak_backend_client",
+    version = "0.1.1",
+    # packages = find_packages(),
+    # scripts = ['say_hello.py'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['requests'],
+
+    # metadata for upload to PyPI
+    author = "Frédéric MOHIER",
+    author_email = "frederic.mohier@gmail.com",
+    description = "Client API for Alignak Backend",
+    license = "GPLv3",
+    keywords = "alignak monitoring",
+    url = "https://github.com/Alignak-monitoring-contrib/alignak-backend-client",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
 )
