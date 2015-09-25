1fb0c9bf22b01762f6f91d3db0d252898ec4e61b
Frédéric MOHIER <frederic.mohier@gmail.com>
2015-09-25 11:47:14 +0000

    For PYPi ...

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@
 from setuptools import setup
 
 setup(
+    name='alignak_backend_client',
+    version='0.1.1',
+    url='https://github.com/Alignak-monitoring-contrib/alignak-backend-client',
     setup_requires=['pbr'],
     pbr=True,
 )
