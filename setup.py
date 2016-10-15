2b71169c033139f0bfafadac5da341ce739bd356
Matthieu Estrada <algorys@users.noreply.github.com>
2016-10-15 23:26:58 +0200

    Add future to install_requires

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
 
-    install_requires=['requests'],
+    install_requires=['requests', 'future'],
 
     classifiers = [
         'Development Status :: 4 - Beta',
